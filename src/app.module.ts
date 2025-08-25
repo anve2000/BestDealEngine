a0f0171fd3e91c26f4a40ec6bcca30b9c8d55f20
anvesha <anvesha@binmile.com>
2025-08-25 14:31:54 +0530

    offers module initialised

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OffersModule } from './apis/offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [OffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
